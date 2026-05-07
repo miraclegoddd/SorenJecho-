ec773a3f05d8c56f52fab37e89e584bf82a56a4b
SorenJecho <drakejapitana3@gmail.com>
2026-05-08 04:27:01 +0800

    Update emotes.lua

--- emotes.lua
+++ emotes.lua
@@ -3,7 +3,7 @@
 local Players = game:GetService("Players")
 local LocalPlayer = Players.LocalPlayer
 
-local CorrectKey = "123" -- Change key here
+local CorrectKey = "000" -- Change key here
 local KeyVerified = false
 
 local CoreGui = game:GetService("CoreGui")
